069e5b065c8219d6331886d8681b7f554935f81a
Mayur845 <90084437+Mayur845@users.noreply.github.com>
2022-04-20 12:48:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,13 +8,3 @@ $(".slider-one")
 	slidesToScroll: 1,
 	autoplaySpeed: 3000
 });
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('https://mayur845.github.io/PWA/serviceworker.js')
-  .then((registration) => {
-  console.log("service worker registered")
-  })
-  .catch((err) => {
-  console.log("sw registraration "+ err)
-  });
-  } 
